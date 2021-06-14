cbd1752ee26df134e7de51edceff9c2a9dadb765
futurelinker <hola@futurelink.me>
2021-06-14 01:32:56 -0500

    Refactor validation

--- 001/app.js
+++ 001/app.js
@@ -23,43 +23,74 @@ function showSucess(input) {
 }
 
 // Check email is valid
-function isValidEmail(email) {
-    const re =
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    // if the passed value matches return 'true'
-      return re.test(String(email).toLowerCase())
+function checkEmail(input) {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  if (!re.test(input.value)) {
+    showError(input, 'Email is not valid')
+  } else {
+    showSucess(input)
+  }
+}
+
+// Check required fields
+function checkRequired(inputArr) {
+  // High order Array method forEach can be attached to any array to do different things
+  inputArr.forEach(function (input) {
+    console.log(input.value)
+    // .trim remove any whitespace
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`)
+    } else {
+      showSucess(input)
+    }
+  })
+}
+
+// Check inputs lenght
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    )
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less than ${max} characters`
+    )
+  } else {
+    showSucess(input)
+  }
+}
+
+// Check passwords match
+function checkPasswordMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, 'Passwords do not match')
+  } else {
+    showSucess(input2)
+  }
+}
+
+// Get Field name
+function getFieldName(input) {
+  // .charAt returns any character based on index
+  // .slice split string in a form an array
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1)
 }
 
 // Event listeners
 // addEventListener represents an object that can handle an event dispatched by an EventTarget object (Element, Document, Window)
 form.addEventListener('submit', (e) => {
   e.preventDefault()
-  console.log(username.value)
+  //   console.log(username.value)
   console.log('submit')
 
-  if (username.value === '') {
-    showError(username, 'Username is required')
-  } else {
-    showSucess(username)
-  }
-
-  if (email.value === '') {
-    showError(email, 'Email is required')
-} else if(!isValidEmail(email.value)) {  
-    showError(email, 'Email is not valid')
-  } else {
-    showSucess(email)
-  }
-  
-  if (password.value === '') {
-    showError(password, 'Password is required')
-  } else {
-    showSucess(password)
-  }
-
-  if (password2.value === '') {
-    showError(password2, 'Password 2 is required')
-  } else {
-    showSucess(password2)
-  }
+  // Check input values
+  checkRequired([username, email, password, password2])
+  checkLength(username, 3, 15)
+  checkLength(password, 8, 25)
+  checkEmail(email)
+  checkPasswordMatch(password, password2)
 })
